504138de77b20e4a1e454cf09aad22a2e1011f6c
samuel diogo <samudiogo@gmail.com>
2016-11-28 00:31:12 -0200

    project before install automapper 5

---
+++ AutomapperMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutomapperMvcDemo.Domain.Entities;
+namespace AutomapperMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private List<Student> StudentMockList()
+        {
+            return new List<Student>
+            {
+                new Student { Id = 1, Name= "Samuel", BirthDate = new DateTime(1990,2,12) },
+                new Student { Id = 1, Name= "Gary", BirthDate = new DateTime(1978,6,21) },
+                new Student { Id = 1, Name= "Andrew", BirthDate = new DateTime(1982,9,6) },
+                new Student { Id = 1, Name= "John", BirthDate = new DateTime(1964,12,18) }
+            };
+        }
+    }
+}
---
+++ AutomapperMvcDemo/Models/StudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutomapperMvcDemo.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name=@"Birth Date")]
+        public DateTime BirthDate { get; set; }
+    }
+}
